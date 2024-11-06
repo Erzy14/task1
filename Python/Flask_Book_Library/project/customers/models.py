39147f4e707442c6178cec56a7f0b26452c48b29
Erzy14 <72360533+Erzy14@users.noreply.github.com>
2024-11-06 18:35:07 +0100

    Update models.py
    Aktualizacja modelu 'Customer'

--- Python/Flask_Book_Library/project/customers/models.py
+++ Python/Flask_Book_Library/project/customers/models.py
@@ -1,5 +1,5 @@
 from project import db, app
-
+import re
 
 # Customer model
 class Customer(db.Model):
@@ -10,6 +10,26 @@ class Customer(db.Model):
     age = db.Column(db.Integer)
 
     def __init__(self, name, city, age):
+
+        if len(name) > 64:
+            name = name[:64]
+            #raise ValueError("Too long name.") #Można albo wywalać błąd, albo obcinać
+
+        if  not re.match(r'^[A-Za-z\s-]*$', name): #Regex na litery małe, duże, spację i -
+            raise ValueError("Name must contain only letters and spaces.") #Tutaj zamiast sanityzacji danych, walimy błędem
+            #name = re.sub(r'[^A-Za-z\s-]', '', name) #Można też usunąć znaki niepasujące
+
+        if len(city) > 64: #Dla polskiego systemu bibliotecznego można byłoby spokojnie dać granicę 40 znaków
+            city = city[:64]
+            #raise ValueError("Too long author.")
+
+        if  not re.match(r'^[A-Za-z\s-]*$', city): #Regex na litery małe, duże, spację i -
+            #raise ValueError("City must contain only letters and spaces.") #Tutaj zamiast sanityzacji danych, walimy błędem
+            city = re.sub(r'[^A-Za-z\s-]', '', city) #Można też usunąć znaki niepasujące
+
+        if (int(age) < 1 or int(age) > 120):
+            raise ValueError("You're so funny. :)")
+
         self.name = name
         self.city = city
         self.age = age
